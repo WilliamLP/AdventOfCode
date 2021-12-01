06c940fa86c014b06f9cf7327ab7ea17c2c46d69
Will Parker <wparker@league.com>
2021-12-01 09:09:20 -0500

    Cleanup

--- 2021/day1.sql
+++ 2021/day1.sql
@@ -4,8 +4,9 @@ WITH vals AS (
         lag(str::INT) OVER (ORDER BY id) AS prev_val
     FROM day1
 )
-SELECT SUM((val > prev_val)::INT) AS num_ascending
-FROM vals;
+SELECT COUNT(*)
+FROM vals
+WHERE val > prev_val;
 
 -- Part 2
 WITH windows AS (
@@ -13,9 +14,11 @@ WITH windows AS (
         SUM(str::INT) OVER (ORDER BY id ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS val
     FROM day1
 ), vals AS (
-    SELECT val, lag(val) OVER (ORDER BY id) AS prev_val
+    SELECT val,
+        lag(val) OVER (ORDER BY id) AS prev_val
     FROM windows
     WHERE id >= 3
 )
-SELECT SUM((val > prev_val)::INT)
-FROM vals;
+SELECT COUNT(*)
+FROM vals
+WHERE val > prev_val;
