4184511ffafd00020fa84f40a9328b8af0206432
Will Parker <wparker@league.com>
2021-12-05 10:19:11 -0500

    Smoosh lines together to make code look shorter

--- 2021/day5.sql
+++ 2021/day5.sql
@@ -4,20 +4,12 @@ WITH parsed AS (
 ), coords AS (
     SELECT coord[1]::INT x1, coord[2]::INT y1, coord[3]::INT x2, coord[4]::INT y2 FROM parsed
 ), vectors AS (
-    SELECT x1, y1, sign(x2-x1) AS dx, sign(y2-y1) AS dy,
-        GREATEST(ABS(x1-x2), ABS(y1-y2)) AS length
-    FROM coords
+    SELECT x1, y1, sign(x2-x1) AS dx, sign(y2-y1) AS dy, GREATEST(ABS(x1-x2), ABS(y1-y2)) AS length FROM coords
 ), points AS (
-    SELECT x1 + i * dx AS x,
-        y1 + i * dy AS y,
-        dx * dy != 0 AS is_diag
-    FROM vectors, generate_series(0, length) AS i
+    SELECT x1 + i * dx AS x, y1 + i * dy AS y, dx * dy != 0 AS is_diag FROM vectors, generate_series(0, length) AS i
 ), multiples_part1 AS (
-    SELECT x, y FROM points WHERE NOT is_diag
-    GROUP BY x, y HAVING COUNT(*) > 1
+    SELECT x, y FROM points WHERE NOT is_diag GROUP BY x, y HAVING COUNT(*) > 1
 ), multiples_part2 AS (
-    SELECT x, y FROM points
-    GROUP BY x, y HAVING COUNT(*) > 1
+    SELECT x, y FROM points GROUP BY x, y HAVING COUNT(*) > 1
 )
-SELECT (SELECT COUNT(*) FROM multiples_part1) AS part1_answer,
-    (SELECT COUNT(*) FROM multiples_part2) AS part2_answer;
+SELECT (SELECT COUNT(*) FROM multiples_part1) AS part1_answer, (SELECT COUNT(*) FROM multiples_part2) AS part2_answer;
