b788db53d58cb242653b020cb12ebb1de62c392c
Will Parker <wparker@league.com>
2021-12-07 05:56:46 -0500

    cleanup day 7

--- 2021/day7.sql
+++ 2021/day7.sql
@@ -1,13 +1,11 @@
-CREATE TEMPORARY TABLE pos AS
-SELECT regexp_split_to_table(str, ',')::INT AS n FROM day7;
-
-WITH series AS (
-    SELECT generate_series(min(n), max(n)) AS center FROM pos
+WITH positions AS (
+    SELECT regexp_split_to_table(str, ',')::INT AS n FROM day7
+), centers AS (
+    SELECT generate_series(min(n), max(n)) AS center FROM positions
 ), fuel_for_center AS (
     SELECT SUM(ABS(n - center)) AS fuel_1,
         SUM((n - center) * (n - center) + ABS(n - center)) / 2 AS fuel_2
-    FROM pos, series
+    FROM positions, centers
     GROUP BY center
 )
-SELECT MIN(fuel_1) AS part1_answer, MIN(fuel_2) AS part2_answer
-FROM fuel_for_center;
+SELECT MIN(fuel_1) AS part1_answer, MIN(fuel_2) AS part2_answer FROM fuel_for_center;
