2f5db53d5507819b4231239f77eaac40f1ac7534
Will Parker <wparker@league.com>
2021-12-20 10:15:56 -0500

    Solved day 20

--- 2021/day20.py
+++ 2021/day20.py
@@ -1,24 +1,33 @@
 from collections import defaultdict
 
+PADDING = 50
+WIDTH = 100 + 2 * PADDING
 
 def parse():
     f = open('day20.txt')
     algo = f.readline().strip()
     f.readline()
-    points = []
-    i = 0
+    points = ['.' * (WIDTH)] * PADDING
     for str in f.readlines():
-        i += 1
-        for j, ch in enumerate(str.strip(), start=1):
-            if ch == '#':
-                points.append((i, j))
+        points.append(('.' * PADDING) + str.strip() + ('.' * PADDING))
+    points.extend(['.' * (WIDTH)] * PADDING)
     return algo, points
 
-def next_points(points, directions, algo):
-    point_agg = defaultdict(lambda: defaultdict(int))
-    for i, j in points:
-        for di, dj, bit in directions:
-            point_agg[i - di][j - dj] += bit
+def next_points(points, directions, algo, inf):
+    res = []
+    for i in range(len(points)):
+        row = ''
+        for j in range(len(points[0])):
+            sum = 0
+            for di, dj, bit in directions:
+                if i + di < 0 or i + di >= WIDTH or j + dj < 0 or j + dj >= WIDTH:
+                    if inf == '#':
+                        sum += bit
+                elif points[i + di][j + dj] == '#':
+                    sum += bit
+            row += algo[sum]
+        res.append(row)
+    return res
 
     res = []
     for i, dict_j in point_agg.items():
@@ -29,14 +38,17 @@ def next_points(points, directions, algo):
     return res
 
 def print_pts(points):
-    grid = []
-    for i in range(-2, 102):
-        grid.append('.' * 104)
-    for i, j in points:
-        grid[i+1] = grid[i+1][0:(j+1)] + '#' + grid[i+1][(j+1)+1:]
-    print('=====')
-    for line in grid:
+    print('=' * (2 * PADDING + WIDTH))
+    for line in points:
         print(line)
+
+def count_points(points):
+    sum = 0
+    for line in points:
+        for ch in line:
+            if ch == '#':
+                sum += 1
+    return sum
 
 def main():
     algo, points = parse()
@@ -45,13 +57,15 @@ def main():
                    (1, -1, 4), (1, 0, 2), (1, 1, 1))
 
     step = 0
-    while step < 2:
-        print_pts(points)
-        points = next_points(points, directions, algo)
+    inf = '.'
+    while step < 50:
+        points = next_points(points, directions, algo, inf)
+        # print_pts(points)
         step += 1
+        inf = algo[0] if inf == '.' else algo[511]
+        if step in (2, 50):
+            print(f'Point count after step {step}: {count_points(points)}')
 
-    print_pts(points)
-    print(f'Part 1 answer: {len(points)}')
 main()
 
 
